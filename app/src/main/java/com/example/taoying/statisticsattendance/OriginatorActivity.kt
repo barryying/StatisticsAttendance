7470ce7836562730bfd774ed3fd7eb9b7719bbbd
TaoYing <barryying>
2018-08-20 17:43:18 +0800

    取消按钮改为finish()

--- app/src/main/java/com/example/taoying/statisticsattendance/OriginatorActivity.kt
+++ app/src/main/java/com/example/taoying/statisticsattendance/OriginatorActivity.kt
@@ -1,16 +1,13 @@
 package com.example.taoying.statisticsattendance
 
 import android.content.Intent
-import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import com.example.taoying.adapter.TestAdapter
 import com.example.taoying.utils.LogUtils
 import com.example.taoying.utils.toast
 import kotlinx.android.synthetic.main.activity_originator.*
-import android.widget.Toast
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
@@ -30,6 +27,7 @@ class OriginatorActivity : AppCompatActivity() {
     //private var adapter: TestAdapter? = null
     var sqliteutils: SqliteUtils? = null
     var listDescriptions: List<Descriptions> = ArrayList()
+    var success = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,9 +35,9 @@ class OriginatorActivity : AppCompatActivity() {
         toast("初始化[一共" + initDB() + "条记录。]")
         //initDB()
 
-        btn_cancel.setOnClickListener { startActivity(Intent(Guiding@this,MainActivity::class.java ))}
+        //btn_cancel.setOnClickListener { startActivity(Intent(Guiding@this,MainActivity::class.java ))}
+        btn_cancel.setOnClickListener { finish() }
         btn_submit.setOnClickListener {
-            var success = false
             // 判断是否为空
             var str_title = edttxt_Title.text.toString()
             var str_place = edttxt_place.text.toString()
@@ -68,7 +66,10 @@ class OriginatorActivity : AppCompatActivity() {
                 initDB()
 
                 //Toast.makeText(this,"创建成功",Toast.LENGTH_SHORT).show()
-                toast("创建成功")
+                if(success == true)
+                    toast("创建成功")
+                else
+                    toast("创建失败")
                 //startActivity(Intent(Guiding@this,MainActivity::class.java ))
             }
         }
